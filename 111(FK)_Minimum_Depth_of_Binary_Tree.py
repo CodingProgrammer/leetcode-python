7d5b8869b2b92c3e72eec3834c797b40b2162cf0
Unknown <157136443@qq.com>
2017-12-23 12:58:05 +0800

    (FK)Min_Depth_of_Binary_Tree

---
+++ 111(FK)_Minimum_Depth_of_Binary_Tree.py
@@ -0,0 +1,17 @@
+ Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if None == root:
+            return 0
+        d, D = sorted(map(self.minDepth,(root.left, root.right)))
+        return 1 + (d or D)
